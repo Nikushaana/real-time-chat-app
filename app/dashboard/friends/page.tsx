435f7b0477c94544986709e52c9e59bb309d74eb
Nikushaana <nikanashvili@gmail.com>
2025-07-17 20:51:58 +0400

    added dashboard pannel

---
+++ app/dashboard/friends/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import InputWithLabel from "@/components/InputWithLabel";
+import { Button } from "@/components/ui/button";
+import { Loader2Icon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function Page() {
+  const router = useRouter();
+  return <div>friends</div>;
+}
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import InputWithLabel from "@/components/InputWithLabel";
+import { Button } from "@/components/ui/button";
+import { Loader2Icon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function Page() {
+  const router = useRouter();
+
+  return (
+    <div>
+      users
+    </div>
+  );
+}
